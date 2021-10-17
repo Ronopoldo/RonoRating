deb6986156fcbb96a21e2c45fe318d2c3c1ccb91
Ivan Filippov <69480820+Ronopoldo@users.noreply.github.com>
2021-10-17 21:40:09 +0000

    /start и начало создания /card

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require("fs");
+const {Canvas} = require("canvacord");
+app.get('/', function(request, response){ response.send(`Монитор активен. Локальный адрес: http://localhost:${port}`); });
+app.listen(port, () => console.log());
+const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+client.on('messageCreate', msg => {
+
+if (msg.content.toLowerCase() == '/start')
+  {
+    let filepath = "./data/UserData/" + msg.author.id;
+    console.log(filepath)
+    if (!fs.existsSync(filepath)) 
+      {
+        fs.mkdir(filepath, err => {console.log(err)})
+      }else
+      {
+        msg.reply('Хей! Ты уже зарегестрирован. Эта команда не для тебя!')
+      }
+  }
+
+
+if (msg.content.toLowerCase().startsWith('/card'))
+  {
+        const args = msg.content.slice(`/био`).split(/ +/);
+    if (args.length >= 2)
+    {
+      pingedUser = args[1]
+      pingedUser = pingedUser.replace("<@",'')
+      pingedUser = pingedUser.replace("!",'')
+      pingedUser = pingedUser.replace(">",'')
+    }
+    if (args.length >= 2)
+    {
+      pingedUser = args[1]
+    }
+    if ((args.length >= 2) && (!args[1].includes('<@')))
+    {
+      pingedUser = '<@' + args[1] + '>'
+      console.log(pingedUser)
+    }
+    if (args.length == 1)
+    {
+      console.log('прошло')
+      pingedUser = '<@' + msg.author.id +">"
+    }
+
+    if (args.length > 2)
+    {    
+      msg.reply('Слишком много аргументов! После команды Вам следует написать лишь один аргумент - упомянание пользователя, его id или вообще не использовать аргументы.\nНе смотря на это, мы всё равно попробуем распознать упомянание')
+    }
+
+
+   if (pingedUser.includes('<@'))
+   {
+     pingedUser = pingedUser.replace("<@",'')
+     pingedUser = pingedUser.replace("!",'')
+     pingedUser = pingedUser.replace(">",'')
+   }else {pingedUser = 'Не найдено пользователей! Используйте упомянание или его id, чтобы указать пользователя'}
+    client.users.fetch(pingedUser).then(User => 
+      {
+      
+        
+        
+        
+        
+
+      },error => {msg.reply('Хей! Что то пошло не так! Убедись, что ты указал верный ID или упомянул существующего пользователя!\nКод ошибки: ' + error)})
+  }
+
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
